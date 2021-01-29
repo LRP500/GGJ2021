40f2ce168627ab21239e2ba485fe1fd068a4218c
Pierre Roy <lrp.roy@gmail.com>
2021-01-29 20:21:27 +0100

    To Unity 2020.2.2f1 + Tools

---
+++ Assets/Gameplay/Tools/Navigation/SceneReference.cs
@@ -0,0 +1,29 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Tools.Navigation
+{
+    [CreateAssetMenu(menuName = "Tools/Navigation/Scene Reference")]
+    public class SceneReference : ScriptableObject
+    {
+#if UNITY_EDITOR
+        public SceneAsset asset;
+#endif
+        public new string name;
+
+        public string path;
+
+#if UNITY_EDITOR
+        private void Refresh()
+        {
+            name = asset.name;
+            path = AssetDatabase.GetAssetPath(asset);
+        }
+#endif
+
+        private void OnValidate()
+        {
+            Refresh();
+        }
+    }
+}
