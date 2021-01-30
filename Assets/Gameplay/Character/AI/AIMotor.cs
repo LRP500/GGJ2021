881be851942918a8dca952065a2fdf159065ed88
Pierre Roy <lrp.roy@gmail.com>
2021-01-30 15:15:18 +0100

    navmesh + AI agent

--- Assets/Gameplay/Character/AI/AIMotor.cs
+++ Assets/Gameplay/Character/AI/AIMotor.cs
@@ -1,8 +1,17 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace GGJ2021
 {
     public class AIMotor : CharacterMovement
     {
+        [SerializeField]
+        private NavMeshAgent _agent;
+
+        public override void Move(Vector3 move, float speed)
+        {
+            _agent.speed = speed;
+            _agent.Move(move * Time.deltaTime);
+        }
     }
 }
--- Assets/Gameplay/Character/AI/States/FollowTargetState.cs
+++ Assets/Gameplay/Character/AI/States/FollowTargetState.cs
@@ -9,15 +9,23 @@ namespace GGJ2021
         [SerializeField]
         private TransformVariable _target;
 
+        [SerializeField]
+        private float _maxDistance = 10f;
+
         public override void Initialize(AIStateMachine character)
         {
         }
 
         public override void Execute(AIStateMachine character)
         {
-            AIMotor motor = character.Motor;
-            Vector3 direction = _target.Value.position - motor.transform.position;
-            motor.Move(direction, motor.WalkSpeed);
+            Vector3 aiPos = character.Motor.transform.position;
+            Vector3 targetPos = _target.Value.position;
+
+            if (Vector3.Distance(aiPos, targetPos) <= _maxDistance)
+            {
+                Vector3 direction = targetPos - aiPos;
+                character.Motor.Move(direction, character.Motor.WalkSpeed);
+            }
         }
     }
 }
