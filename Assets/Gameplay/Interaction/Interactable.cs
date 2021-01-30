d1703d90ed40bef41b6ac4e7f8ff1e6bd17476aa
Pierre Roy <lrp.roy@gmail.com>
2021-01-30 20:19:41 +0100

    FIX Interactable

--- Assets/Gameplay/Interaction/Interactable.cs
+++ Assets/Gameplay/Interaction/Interactable.cs
@@ -1,10 +1,29 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace GGJ2021
 {
-    public abstract class Interactable : MonoBehaviour
+    public class Interactable : MonoBehaviour
     {
-        public abstract void Interact();
+        [SerializeField]
+        private string _label = string.Empty;
+        public string Label => _label;
+
+        [SerializeField]
+        private float _range = 1f;
+        public float Range => _range;
+
+        [SerializeField]
+        private bool _canBeInteractedWith = true;
+
+        [SerializeField]
+        private Action _action = null;
+
+        public void Interact()
+        {
+            if (_canBeInteractedWith)
+            {
+                _action.Execute();
+            }
+        }
     }
 }
